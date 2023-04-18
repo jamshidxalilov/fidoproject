dca9d6216f19e45b9f92ddb344f06e886e85e23b
Jamshid Xalilov <jajuxalilov@gmail.com>
2023-04-18 08:11:20 +0500

    Month model added

--- main/models.py
+++ main/models.py
@@ -72,3 +72,22 @@ class Attendance(models.Model):
     class Meta:
         verbose_name = ('Ish davomati')
         verbose_name_plural = ('Ish davomati')
+
+
+class Month(models.Model):
+    MONTH = (
+        (1, ("Yanvar")),
+        (2, ("Fevral")),
+        (3, ("Mart")),
+        (4, ("Aprel")),
+        (5, ("May")),
+        (6, ("Iyun")),
+        (7, ("Iyul")),
+        (8, ("Avgust")),
+        (9, ("Sentyabr")),
+        (10, ("Oktyabr")),
+        (11, ("Noyabr")),
+        (12, ("Dekabr")),
+    )
+
+    month = models.SmallIntegerField(default=100)
