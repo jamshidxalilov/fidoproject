23b5b8d44e0cfd2a27d214e5a578c299a5f587d3
Jamshid Xalilov <jajuxalilov@gmail.com>
2023-04-18 12:00:08 +0500

    ...

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from datetime import datetime, date, timezone
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView, DetailView
-from django.db.models import Sum
+from django.db.models import Sum, F
 from django_filters.views import FilterView
 
 from main.filter import AttendanceFilter
@@ -34,6 +34,14 @@ class StaffAbout(DetailView):
         data['months'] = Months.objects.all()
         data['attendance'] = Attendance.objects.filter(staff_id=pk, enter_at__month=str(month)).order_by("-id")
         data['total_hours'] = [Attendance.objects.filter(staff_id=pk, enter_at__month=str(month)).aggregate(Sum('hours'))]
+        salary = Staff.objects.filter(id=self.object.pk).values('hour_salary')
+        # t_hours = Attendance.objects.filter(staff_id=pk, enter_at__month=str(month)).\
+        #     values('staff__hour_salary', 'hours').annotate(total=F('hours') * F('staff__hour_salary'))
+        # data['t_hours'] = t_hours
+        t_h = data['total_hours'][0]['hours__sum']
+        s_l = list(salary)[0]['hour_salary']
+        data['summa'] = t_h / 3600 * int(s_l)
+
         # data['attendance_hours'] = Attendance.objects.filter(staff_id=pk, action_at__gte=date(2023, 5, 1))
         return data
 
